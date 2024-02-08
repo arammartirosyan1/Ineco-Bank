e902e9b56c3973fd5d00e956129dd46049a41ce2
arammartirosyan1 <arammartirosyan788@gmail.com>
2024-02-09 01:20:01 +0400

    date

--- ClassBank.py
+++ ClassBank.py
@@ -6,6 +6,16 @@ from dateutil.relativedelta import relativedelta
 import sys
 
 
+class Error(Exception):
+
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+
+    def __repr__(self):
+        return "Wrong for {} : {}".format(self.message, self.value)
+
+
 # cd C:\Users\aramm\'OneDrive - EFES ICJSC'\Desktop\INECO BANK
 # python translation.py Ineco I1
 
@@ -156,6 +166,10 @@ def ben_true():
                     if i == 0:
                         x = str(data[i]).split()
                         ls_1["IS_INSURED_PHYSICAL"] = "1"
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 1:
                         if str(data[1]).split() == str(data[2]).split():
                             pass
@@ -163,16 +177,27 @@ def ben_true():
                             x = str(data[i]).split()
                             ls_1["PROPERTY_OWNER_PERSON_FIRST_NAME"] = x[0]
                             ls_1["PROPERTY_OWNER_PERSON_LAST_NAME"] = x[1]
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 2:
                         x = str(data[i]).split()
                         ls_1["INSURED_NAME"] = x[0]
                         ls_1["INSURED_LAST_NAME"] = x[1]
                         ls_1["INSURED_SECOND_NAME"] = x[2]
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 3:
                         x = str(data[i]).split()[0]
                         x = datetime.datetime.strptime(x, "%Y-%m-%d").strftime("%d.%m.%Y")
                         ls_1["INSURED_BIRTHDAY"] = x
+                except:
+                    print("Error", data[i])
 
+                try:
                     if i == 4:
                         x = str(data[i]).split()
                         ls_1["INSURED_SOCIAL_CARD"] = x[0]
@@ -180,6 +205,10 @@ def ben_true():
                             ls_1["INSURED_GENDER"] = "F"
                         else:
                             ls_1["INSURED_GENDER"] = "M"
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 5:
                         x = str(data[i]).split()
                         ls_1["INSURED_CITIZENSHIP"] = "ՀՀ"
@@ -189,6 +218,10 @@ def ben_true():
                         start = datetime.datetime.strptime(x[1], "%d/%m/%Y")
                         end = start + relativedelta(years=+10)
                         ls_1["INSURED_PASSPORT_EXPIRY_DATE"] = end.strftime("%d.%m.%Y")
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 6:
                         x = str(data[i]).split()[0]
                         if len(x) == 9:
@@ -196,9 +229,17 @@ def ben_true():
                         else:
                             x = "0" + x[::-1][:8][::-1]
                             ls_1["INSURED_MOBILE_PHONE"] = x
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 7:
                         x = str(data[i]).split()
                         ls_1["INSURED_MAIL"] = x[0]
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 8:
                         new_data = str(data[i]).replace(',', " ").strip()
                         new_data = new_data.split(" ")
@@ -217,7 +258,18 @@ def ben_true():
                                         ls_1['INSURED_REG_CITY'] = "Այլ"
                                     else:
                                         ls_1['INSURED_REG_CITY'] = j
+                except:
+                    if i == 8:
+                        new_data_1 = str(new_data_1).strip().strip(",").strip()
+                        ls_1["INSURED_REG_COUNTRY"] = "ARM"
+                        ls_1["INSURED_REG_REGION"] = j
+                        ls_1['INSURED_REG_FULL_ADDRESS'] = new_data_1
+                        if j == "Տավուշ":
+                            ls_1['INSURED_REG_CITY'] = "Այլ"
+                        else:
+                            ls_1['INSURED_REG_CITY'] = j
 
+                try:
                     if i == 9:
                         new_data = str(data[i]).replace(',', " ").strip()
                         new_data = new_data.split(" ")
@@ -237,19 +289,41 @@ def ben_true():
                                     else:
                                         ls_1['INSURED_LIVE_CITY'] = j
                                         i += 1
+                except:
+                    if i == 9:
+                        try:
+                            new_data_1 = str(new_data_1).strip().strip(",").strip()
+                            ls_1["INSURED_LIVE_COUNTRY"] = "ARM"
+                            ls_1["INSURED_LIVE_REGION"] = j
+                            ls_1['INSURED_LIVE_FULL_ADDRESS'] = new_data_1
+                            if j == "Տավուշ":
+                                ls_1['INSURED_LIVE_CITY'] = "Այլ"
+                            else:
+                                ls_1['INSURED_LIVE_CITY'] = j
+                        except:
+                            ls_1["IS_INSURED_SAME_REG_LIVE_ADDRESS"] = '1'
+
                     if i == 10:
                         pass
                     if i == 11:
                         pass
+
+                try:
                     if i == 12:
                         x = str(data[12]).split()
                         ls_1["PROPERTY_NAME"] = x[0]
                         ls_1["PROPERTY_TYPE"] = x[0]
+                except:
+                    print("Error", data[i])
 
+                try:
                     if i == 13:
                         x = str(data[13]).split()
-
                         ls_1["PROPERTY_MARKET_PRICE"] = x[0]
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 14:
                         x = str(data[14]).split()
                         ls_1['PROPERTY_RISK_AMOUNT'] = x[0]
@@ -258,8 +332,13 @@ def ben_true():
                         ls_1['PROPERTY_RISK_PREMIUM'] = risk
                         ls_1["POLICY_AMOUNT_CURRENCY"] = "AMD"
                         ls_1["PROPERTY_INSURANCE_RATE"] = "0.16"
+                except:
+                    print("Error", data[i])
+
                     if i == 15:
                         pass
+
+                try:
                     if i == 16:
                         new_data = str(data[i]).replace(',', " ").strip()
                         new_data = new_data.split(" ")
@@ -278,43 +357,8 @@ def ben_true():
                                         ls_1['PROPERTY_CITY'] = "Այլ"
                                     else:
                                         ls_1['PROPERTY_CITY'] = j
-                    if i == 17:
-                        x = str(data[i]).split()[0]
-                        if len(x) > 5:
-                            ls_1["PROPERTY_OWNER_PERSON_SOCIAL_CARD"] = x
-                            ls_1["PROPERTY_OWNER_PERSON_PERS_BPR_USE"] = "1"
-                        else:
-                            ls_1["IS_OWNER_PERSON_SAME_INSURED"] = "1"
 
-                    if i == 18:
-                        x = str(data[i]).split()[0]
-                        start = datetime.datetime.strptime(x, "%Y-%m-%d")
-                        end = start + datetime.timedelta(days=365)
-                        ls_1["POLICY_FROM_DATE"] = start.strftime("%d.%m.%Y")
-                        ls_1["POLICY_TO_DATE"] = end.strftime("%d.%m.%Y")
                 except:
-                    if i == 8:
-                        new_data_1 = str(new_data_1).strip().strip(",").strip()
-                        ls_1["INSURED_REG_COUNTRY"] = "ARM"
-                        ls_1["INSURED_REG_REGION"] = j
-                        ls_1['INSURED_REG_FULL_ADDRESS'] = new_data_1
-                        if j == "Տավուշ":
-                            ls_1['INSURED_REG_CITY'] = "Այլ"
-                        else:
-                            ls_1['INSURED_REG_CITY'] = j
-
-                    if i == 9:
-                        try:
-                            new_data_1 = str(new_data_1).strip().strip(",").strip()
-                            ls_1["INSURED_LIVE_COUNTRY"] = "ARM"
-                            ls_1["INSURED_LIVE_REGION"] = j
-                            ls_1['INSURED_LIVE_FULL_ADDRESS'] = new_data_1
-                            if j == "Տավուշ":
-                                ls_1['INSURED_LIVE_CITY'] = "Այլ"
-                            else:
-                                ls_1['INSURED_LIVE_CITY'] = j
-                        except:
-                            ls_1["IS_INSURED_SAME_REG_LIVE_ADDRESS"] = '1'
                     if i == 16:
                         new_data_1 = str(new_data_1).strip().strip(",").strip()
                         ls_1["PROPERTY_COUNTRY"] = "ARM"
@@ -325,7 +369,40 @@ def ben_true():
                         else:
                             ls_1['PROPERTY_CITY'] = j
 
+                try:
+                    if i == 17:
+                        # Եթե սեփականատեր դաշտը լրացված է(գրված է սոց քարտ) գրում ենք այդ տվերը և BPR դաշտում գրում 1
+                        x = str(data[i]).split()[0]
+                        if len(x) > 5:
+                            ls_1["PROPERTY_OWNER_PERSON_SOCIAL_CARD"] = x
+                            ls_1["PROPERTY_OWNER_PERSON_PERS_BPR_USE"] = "1"
+                        else:
+                            # Եթե սեփականատեր դաշտը բաց է թողվաց գրում ենք սեփականատեր հանդիսանում է ապահովադիրը
+                            ls_1["IS_OWNER_PERSON_SAME_INSURED"] = "1"
+                except:
+                    print("Error", data[i])
+
+                try:
                     if i == 18:
+                        # Եթե նշված է օր վերցնում ենք այդ օրը որպես պայմանագրի սկիզբ
+                        x = str(data[i]).split()[0]
+                        start_send = datetime.datetime.strptime(x, "%Y-%m-%d")
+                        end = start_send + datetime.timedelta(days=365)
+                        ls_1["POLICY_FROM_DATE"] = start_send.strftime("%d.%m.%Y")
+                        ls_1["POLICY_TO_DATE"] = end.strftime("%d.%m.%Y")
+
+                        # Եթե նշված է օր վերցնում ենք հաջորդ ամսվա այդ օրը
+                        start_date = datetime.date.today().strftime("%d.%m.%Y")
+                        start = datetime.datetime.strptime(start_date, "%d.%m.%Y")
+                        end_date = start + relativedelta(months=+1)
+                        end = end_date.strftime("%d.%m.%Y")
+                        payment = str(risk), end
+                        ls_1["POLICY_CREATION_DATE"] = start_date
+                        ls_1["POLICY_PAYMENT_SCHEDULE"] = ", ".join(payment)
+
+                except:
+                    if i == 18:
+                        # Եթե նշված չի օր վերցնում ենք այս օրը որպես պայմանագրի սկիզբ
                         start_date = datetime.date.today()
                         end_date = start_date + datetime.timedelta(days=365)
                         start = start_date.strftime("%d.%m.%Y")
@@ -333,18 +410,14 @@ def ben_true():
                         ls_1["POLICY_FROM_DATE"] = start
                         ls_1["POLICY_TO_DATE"] = end
 
-    ####################################
-    start_date = datetime.date.today().strftime("%d.%m.%Y")
-    print(str(start_date))
-    ls_1["POLICY_CREATION_DATE"] = start_date
-    ls_1["POLICY_PAYMENT_SCHEDULE"] = str(risk + ", " + start_date)
-    num = int(start_date.split('.')[0])
-    start_date = datetime.date.today()
-    while num != 15:
-        num += 1
-        start_date = start_date + datetime.timedelta(days=1)
-
-    print(str(risk + ", " + str(start_date)))
+                        # Եթե նշված չի օր վերցնում ենք գալիք ամսվա 15ը
+                        start_date = datetime.date.today().strftime("%d.%m.%Y")
+                        start = datetime.datetime.strptime(start_date, "%d.%m.%Y")
+                        end_date = start + relativedelta(months=+1)
+                        end = end_date.strftime("15.%m.%Y")
+                        payment = str(risk), end
+                        ls_1["POLICY_CREATION_DATE"] = start_date
+                        ls_1["POLICY_PAYMENT_SCHEDULE"] = ", ".join(payment)
 
 
     ##########################
@@ -363,5 +436,3 @@ def ben_true():
 
 
 bank = Bank('Ineco', 'I1')
-
-
